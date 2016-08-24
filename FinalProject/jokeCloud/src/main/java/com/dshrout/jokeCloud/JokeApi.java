91b3e2036a019a3726a6754ae75fb91464719f0b
dshrout <madwhit@gmail.com>
2016-08-24 12:52:25 -0500

    Backend working locally.

--- FinalProject/jokeCloud/src/main/java/com/dshrout/jokeCloud/JokeApi.java
+++ FinalProject/jokeCloud/src/main/java/com/dshrout/jokeCloud/JokeApi.java
@@ -6,6 +6,7 @@
 
 package com.dshrout.jokeCloud;
 
+import com.dshrout.fungi.JokeSmith;
 import com.google.api.server.spi.config.Api;
 import com.google.api.server.spi.config.ApiMethod;
 import com.google.api.server.spi.config.ApiNamespace;
@@ -24,43 +25,20 @@ import java.util.Random;
   )
 )
 public class JokeApi {
-    private String[] answers;
-    private Random mRandom = null;
-
-    private void InitJokeApi() {
-        answers = new String[8];
-        answers[0] = "42\n-Deep Thought";
-        answers[1] = "Aliens\n-Giorgio A. Tsoukalos";
-        answers[2] = "Because you didn't cook it!\n-Gordon Ramsay";
-        answers[3] = "Nobody cares\n-Grumpy Cat";
-        answers[4] = "There is no road\n-Spoon Boy";
-        answers[5] = "Never send a chicken to do a machine's job\n-Agent Smith";
-        answers[6] = "To join the collective\n-Borg";
-        answers[7] = "PC LOAD LETTER";
-        mRandom = new Random();
-    }
-
     /** A simple endpoint method that takes a name and says Hi back */
     @ApiMethod(name = "sayHi")
     public JokeData sayHi(@Named("name") String name) {
-        if (mRandom == null) {
-            InitJokeApi();
-        }
-
         JokeData response = new JokeData();
         response.setData("Hi, " + name);
-
         return response;
     }
 
     /** A simple endpoint method that returns a random answer to the eternal question: Why'd the chicken cross the road? */
     @ApiMethod(name = "jokePlease")
     public JokeData jokePlease() {
-        if (mRandom == null) {
-            InitJokeApi();
-        }
+        JokeSmith jokeSmith = new JokeSmith();
         JokeData jokeData = new JokeData();
-        jokeData.setData(answers[mRandom.nextInt(answers.length)]);
+        jokeData.setData(jokeSmith.getJoke());
         return jokeData;
     }
 }
--- FinalProject/jokeFoundry/src/main/java/com/dshrout/fungi/JokeSmith.java
+++ FinalProject/jokeFoundry/src/main/java/com/dshrout/fungi/JokeSmith.java
@@ -1,7 +1,25 @@
 package com.dshrout.fungi;
 
+import java.util.Random;
+
 public class JokeSmith {
-    public JokeVM getJoke(){
-        return new JokeVM("What do you call a smart blond?", "A Golden Retriever!");
+    private String[] mAnswers;
+    private Random mRandom;
+
+    public JokeSmith() {
+        mAnswers = new String[8];
+        mAnswers[0] = "42\n-Deep Thought";
+        mAnswers[1] = "Aliens\n-Giorgio A. Tsoukalos";
+        mAnswers[2] = "Because you didn't cook it!\n-Gordon Ramsay";
+        mAnswers[3] = "Nobody cares\n-Grumpy Cat";
+        mAnswers[4] = "There is no chicken\n-Spoon Boy";
+        mAnswers[5] = "Never send a chicken to do a machine's job\n-Agent Smith";
+        mAnswers[6] = "To join the collective\n-Borg";
+        mAnswers[7] = "PC LOAD LETTER";
+        mRandom = new Random();
+    }
+
+    public String getJoke() {
+        return mAnswers[mRandom.nextInt(mAnswers.length)];
     }
 }
