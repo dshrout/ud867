6ca08ccd2e7de0de6f1f87c71c15c26f0e1f911d
dshrout <madwhit@gmail.com>
2016-08-24 15:51:01 -0500

    Step 3: Complete!

--- FinalProject/app/src/main/java/com/udacity/gradle/builditbigger/EndpointsAsyncTask.java
+++ FinalProject/app/src/main/java/com/udacity/gradle/builditbigger/EndpointsAsyncTask.java
@@ -21,18 +21,18 @@ class EndpointsAsyncTask extends AsyncTask<Void, Void, String> {
     @Override
     protected String doInBackground(Void... params) {
         if(myApiService == null) {  // Only do this once
-            MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(),
-                    new AndroidJsonFactory(), null)
-                    // options for running against local devappserver
-                    // - 10.0.2.2 is localhost's IP address in Android emulator
-                    // - turn off compression when running against local devappserver
-                    .setRootUrl("http://192.168.1.156:8080/_ah/api/")
-                    .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
-                        @Override
-                        public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
-                            abstractGoogleClientRequest.setDisableGZipContent(true);
-                        }
-                    });
+            MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(), new AndroidJsonFactory(), null)
+                                             .setRootUrl("https://jokeengine-122668.appspot.com/_ah/api/");
+            // options for running against local devappserver
+            // - 10.0.2.2 is localhost's IP address in Android emulator
+            // - turn off compression when running against local devappserver
+//                    .setRootUrl("http://192.168.1.156:8080/_ah/api/")
+//                    .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
+//                        @Override
+//                        public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
+//                            abstractGoogleClientRequest.setDisableGZipContent(true);
+//                        }
+//                    });
             // end options for devappserver
 
             myApiService = builder.build();
--- FinalProject/jokeCloud/src/main/java/com/dshrout/jokeCloud/JokeApi.java
+++ FinalProject/jokeCloud/src/main/java/com/dshrout/jokeCloud/JokeApi.java
@@ -10,7 +10,6 @@ import com.dshrout.fungi.JokeSmith;
 import com.google.api.server.spi.config.Api;
 import com.google.api.server.spi.config.ApiMethod;
 import com.google.api.server.spi.config.ApiNamespace;
-import com.google.api.server.spi.config.Named;
 
 import java.util.Random;
 
@@ -25,14 +24,6 @@ import java.util.Random;
   )
 )
 public class JokeApi {
-    /** A simple endpoint method that takes a name and says Hi back */
-    @ApiMethod(name = "sayHi")
-    public JokeData sayHi(@Named("name") String name) {
-        JokeData response = new JokeData();
-        response.setData("Hi, " + name);
-        return response;
-    }
-
     /** A simple endpoint method that returns a random answer to the eternal question: Why'd the chicken cross the road? */
     @ApiMethod(name = "jokePlease")
     public JokeData jokePlease() {
