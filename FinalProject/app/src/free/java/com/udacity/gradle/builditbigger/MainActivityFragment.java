e48e8069bbb08df1e8f8e25fea5e6e5a29c22024
dshrout <madwhit@gmail.com>
2016-08-25 08:36:33 -0500

    Product Flavors working. Need to remove unnecessary dependencies from paid version.

--- FinalProject/app/src/free/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
+++ FinalProject/app/src/free/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
@@ -1,7 +1,8 @@
 package com.udacity.gradle.builditbigger;
 
-import android.support.v4.app.Fragment;
+
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,12 +12,15 @@ import com.google.android.gms.ads.AdView;
 
 
 /**
- * A placeholder fragment containing a simple view.
+ * A simple {@link Fragment} subclass.
  */
 public class MainActivityFragment extends Fragment {
 
+
     public MainActivityFragment() {
+        // Required empty public constructor
     }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
